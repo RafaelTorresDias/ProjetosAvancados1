6d8603dbc05bfb4400a012c88db64dc7eee5a926
RafaelTorresDias <rafaeltorres@ufcspa.edu.br>
2017-04-03 17:02:11 -0300

    Alteração Ingrediente linha 70
    Alteração Ingrediente Linha 70 de Presunto para Calabresa

--- Pizza/src/pizzaria/Pizza.java
+++ Pizza/src/pizzaria/Pizza.java
@@ -67,7 +67,7 @@ public class Pizza {
     public static void main(String[] args) {
         Pizza p = new Pizza();
         p.adiconarIngrediente("Queijo");
-        p.adiconarIngrediente("Presunto");
+        p.adiconarIngrediente("Calabresa");
         System.out.println(p.getListaDeIngredientes());
 
     }
